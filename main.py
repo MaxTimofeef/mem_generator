f692faf740a8307422c4fb0f463f3e69dcc6f25b
maxim <ebanancik9@gmail.com>
2025-03-13 19:25:54 +0500

    feat: add choice image

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 print("Генератор мемов запущен!")
+print("Список картинок: ")
+print("1.кот в ресторане")
+print("2.Кот в очках")
+
+user_answer = int(input("Выберите картинку цифрой"))
 
 top_text = input("Введите верхний текст: ")
 bottom_text = input("Введите нижний текст: ")
+
+
+if user_answer == 1:
+    image = "Кот в очках.png"
+elif user_answer == 2:
+    image = "Кот в ресторане.png"
